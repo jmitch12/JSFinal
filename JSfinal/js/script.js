35a7d87578c9f881aac7348b39a81ddc81e6a20d
jmitch12 <mitcj785@newschool.edu>
2015-10-06 15:10:44 -0400

    API

--- JSfinal/js/script.js
+++ JSfinal/js/script.js
@@ -7,6 +7,8 @@ var audioElement = document.createElement('audio');
 audioElement.setAttribute('src', 'audio/audio.mp3');
 var circulate = $('#circulate');
 
+//First heart animation
+
 $(heart2).click(function(){
 	$(heart2).animate({ 
 		width: '120%',
@@ -23,13 +25,30 @@ $(heart2).click(function(){
 
 });
 
+//circulation animation
 
     $(circulate).addClass('animated fadeIn');
 
+ //NY Times API
 
+var article1 = (function () {
+	    $.ajax({
+	        'async': false,
+	        'global': false,
+	        'url': "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=human+heart+health&sort=newest&api-key=87952c56194122e2acf6ca04a92d5a50%3A5%3A73137553",
+	        'dataType': "json",
+	        'success': function (data) {
+	        	console.log(data);
+	            var article1 = data.response.docs[1].web_url;
+	            var article1title = data.response.docs[1].snippet;
+	            document.getElementById('nytimes').innerHTML = '<a href="' + article1 + '">'+ article1title +'</a>';
+	            var article2 = data.response.docs[4].web_url;
+	            var article2title = data.response.docs[4].snippet;
+	            document.getElementById('nytimes2').innerHTML = '<a href="' + article2 + '">'+ article2title +'</a>';
+	   
+	        }
+	    });
+	    return article1;
+	})(); 
 
-// new Vivus('arrow', {
-//     type: 'delayed',
-//     duration: 200,
-//     animTimingFunction: Vivus.EASE
-// }, myCallback);
+	
